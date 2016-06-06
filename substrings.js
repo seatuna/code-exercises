73988ec25e53ae6c8b7972e9bb35998be2a2cc75
Celena T <chibiynm@gmail.com>
2016-06-06 11:22:53 -0400

    Clarified error

--- substrings.js
+++ substrings.js
@@ -1,16 +1,21 @@
 'use strict';
 
+// This doesn't work for all cases yet, will need to fix this!
+// Currently has an error with indexOf
 // Given 2 strings, determine whether any substrings that exist in both strings
 
 let substring = function(str1, str2) {
   let results = [];
   let longest;
+  let shortest;
 
   // determines which string is longer
   if(str1.length > str2.length) {
     longest = str1;
+    shortest = str2;
   } else {
     longest = str2;
+    shortest = str1;
   }
 
   //function for getting all possible substrings of a string
@@ -21,20 +26,20 @@ let substring = function(str1, str2) {
       strArray.push(string.substring(i));
     }
 
-    return strArray;
+    console.log(strArray);
   }
 
   //function to determine whether a substring in str1 exists in str2
-  let substrings1 = allSubstrings(str1);
-  let substrings2 = allSubstrings(str2);
+  let substrings1 = allSubstrings(longest);
+  let substrings2 = allSubstrings(shortest);
 
   for(let i = 0; i < longest.length; i++) {
-    if(substrings1.indexOf(substrings2[i]) !== -1) {
-      results.push(substrings2[i]);
+    if(substrings2.indexOf(substrings1[i]) !== -1) {
+      results.push(substrings1[i]);
     }
   }
 
   console.log(results);
 }
 
-substring('blahblahblah','i cannot sleep blah');
+substring('blahblahblah what is sleep idk','i cannot sleep blah');
